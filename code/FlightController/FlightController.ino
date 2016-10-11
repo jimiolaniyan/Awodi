a887d351cb8b63cd0a3a4421d568afc1e39cd1c8
David Emukpere <davidemukpere@gmail.com>
2016-10-11 14:32:59 +0100

    Wireless arduino programming

--- code/FlightController/FlightController.ino
+++ code/FlightController/FlightController.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 #define TURN_OFF 1100
-#define STEP 10
-#define START 1230
+#define STEP 50
+#define START 1700
 int arm_time = 0;
 int count = 500;
 int Pulse = 1000;
@@ -12,7 +12,7 @@ Servo servo3, servo4, servo5, servo7;   //this should be changed to front, right
 void setup() {
   // put your setup code here, to run once:
   
-  Serial.begin(9600);
+  Serial.begin(57600);
   DDRD  |=  (_BV(PORTD3)) | (_BV(PORTD7)) | (_BV(PORTD4)) | (_BV(PORTD5));
   for (arm_time = 0; arm_time < count; ++arm_time){
     PORTD |= (1 << PORTD7 ) | (1 << PORTD3) | (1 << PORTD4 ) | (1 << PORTD5);
@@ -21,13 +21,13 @@ void setup() {
     delay(20 - (Pulse/1000));
   }
   
-  Pulse3 = Pulse4 = Pulse5 = Pulse7 = START;
-  servo3.attach(3);
-  servo4.attach(4);
-  servo5.attach(5);
-  servo7.attach(7);
+  //Pulse3 = Pulse4 = Pulse5 = Pulse7 = START;
+  //servo3.attach(3);
+  //servo4.attach(4);
+  //servo5.attach(5);
+  //servo7.attach(7);
   
-  //Pulse = START;
+  Pulse = START;
 }
 
 void spin(const int &, const int &, const int &, const int &);
@@ -39,20 +39,20 @@ void loop() {
      
      switch (inByte) {
        case '*':
-         Pulse3 = Pulse4 = Pulse5 = Pulse7 = TURN_OFF;
-         //Pulse = TURN_OFF;
+         //Pulse3 = Pulse4 = Pulse5 = Pulse7 = TURN_OFF;
+         Pulse = TURN_OFF;
          break;
        case '+':
-         Pulse3 = Pulse4 = Pulse5 = Pulse7 += STEP;
-         //Pulse += STEP;
+         //Pulse3 = Pulse4 = Pulse5 = Pulse7 += STEP;
+         Pulse += STEP;
          break;
        case '-':
-         Pulse3 = Pulse4 = Pulse5 = Pulse7 -= STEP;
-         //Pulse -= STEP;
+         //Pulse3 = Pulse4 = Pulse5 = Pulse7 -= STEP;
+         Pulse -= STEP;
          break;
        case 's':
-         Pulse3 = Pulse4 = Pulse5 = Pulse7 = START;
-         //Pulse = START;
+         //Pulse7 = Pulse4 = Pulse5 = Pulse3 = START;
+         Pulse = START;
          break;
        case 'r':
          Pulse4 -= STEP;
@@ -60,19 +60,19 @@ void loop() {
          Pulse3 = Pulse7 = TURN_OFF;
          break;
        case 'p':
-         Pulse3 -= STEP;
-         Pulse7 += STEP;
+         Pulse3 += STEP;
+         Pulse7 -= STEP;
          Pulse4 = Pulse5 = TURN_OFF;
          break;   
      }
    }
    
-   spin(Pulse3, Pulse4, Pulse5, Pulse7);
+   //spin(Pulse3, Pulse4, Pulse5, Pulse7);
   
-  /*PORTD |= (1 << PORTD7 ) | (1 << PORTD3) | (1 << PORTD4 ) | (1 << PORTD5);
+  PORTD |= (1 << PORTD7 ) | (1 << PORTD3) | (1 << PORTD4 ) | (1 << PORTD5);
   delayMicroseconds(Pulse);
   PORTD &= ~(_BV(PORTD3)) & ~(_BV(PORTD7)) & ~(_BV(PORTD4)) & ~(_BV(PORTD5));
-  delay(20 - (Pulse/1000));*/
+  delay(20 - (Pulse/1000));
 }
 
 void spin(const int &Pulse3, const int &Pulse4, const int &Pulse5, const int &Pulse7) {
